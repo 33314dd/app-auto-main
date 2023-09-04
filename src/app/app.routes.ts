2651de098703b8e522d1d9d92709bbf2a030665e
unknown <die.valenzuelac@duocuc.cl>
2023-09-04 14:58:16 -0300

    segundo commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch:'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'cliente/:id',
+    loadComponent: () => import('./cliente/cliente.page').then( m => m.ClientePage)
+  },
+  {
+    path: 'cliente',
+    loadComponent: () => import('./cliente/cliente.page').then( m => m.ClientePage)
+  },
+  {
+    path: 'admin',
+    loadComponent: () => import('./admin/admin.page').then( m => m.AdminPage)
+  }, 
+  {
+    path: 'conductor',
+    loadComponent: () => import('./conductor/conductor.page').then( m => m.ConductorPage)
+  },
+ 
+];
